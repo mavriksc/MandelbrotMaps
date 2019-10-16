08afd2413dd516aa376b424d59d73067a2af35c9
Sean Carlisle <scarlisle@hylamobile.com>
2019-10-16 16:37:36 -0500

    some good stuffs. landscaped. and imaginary numbers

--- app/src/main/java/org/mavriksc/mandelbrotmaps/MainActivity.kt
+++ app/src/main/java/org/mavriksc/mandelbrotmaps/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.Handler
 import android.widget.ImageView
 import androidx.core.graphics.set
 import androidx.core.view.drawToBitmap
+import org.mavriksc.mandelbrotmaps.type.ImaginaryNumber
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +17,14 @@ class MainActivity : AppCompatActivity() {
     private val colorView: ImageView by lazy { findViewById<ImageView>(R.id.colorView) }
 
     private val bitmap: Bitmap by lazy { colorView.drawToBitmap() }
+
+    private val MAX_ITER = 20
+
+    private val colorMap: Map<Int, Int> = mapOf(1 to Color.WHITE,2 to Color.CYAN,3 to Color.BLUE, 4 to Color.GREEN, 5 to Color.YELLOW)
+
+    private val initialRealRange = -2..1
+    private val initialImaginaryRange = -1..1
+
     private lateinit var task: Runnable
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,5 +46,31 @@ class MainActivity : AppCompatActivity() {
             mHandler.post(task)
         }
         mHandler.postDelayed(task, 500)
+        testImaginaryNums()
     }
+
+    private fun testImaginaryNums() {
+        val x = ImaginaryNumber(1.0, 1.0)
+        val y = ImaginaryNumber(2.0, 2.0)
+        println("x=$x")
+        println("y=$y")
+        println("||X|| = " + x.magnitude())
+        println("||Y|| = " + y.magnitude())
+        println(x + y)
+        println(y * y)
+        println(getCount(x))
+        println(getCount(y))
+
+    }
+
+    private fun getCount(coord: ImaginaryNumber): Int {
+        var z = ImaginaryNumber(0.0, 0.0)
+        var count = 0
+        while (z.magnitude() < 2 && count < MAX_ITER) {
+            z = z * z + coord
+            count++
+        }
+        return count
+    }
+
 }
---
+++ app/src/main/java/org/mavriksc/mandelbrotmaps/type/ImaginaryNumber.kt
@@ -0,0 +1,23 @@
+package org.mavriksc.mandelbrotmaps.type
+
+import kotlin.math.sqrt
+
+class ImaginaryNumber(val real: Double = 0.0, val imaginary: Double = 0.0) {
+
+    fun magnitude(): Double = sqrt(real * real + imaginary * imaginary)
+
+    operator fun plus(other: ImaginaryNumber): ImaginaryNumber =
+        ImaginaryNumber(real + other.real, imaginary + other.imaginary)
+
+    operator fun times(other: ImaginaryNumber): ImaginaryNumber {
+        val newReal = real * other.real - imaginary * other.imaginary
+        val newImaginary = real * other.imaginary + imaginary * other.real
+        return ImaginaryNumber(newReal, newImaginary)
+    }
+
+    override fun toString(): String {
+        val join = if (imaginary < 0) "-" else "+"
+        return "$real $join $imaginary" + "i"
+    }
+
+}
